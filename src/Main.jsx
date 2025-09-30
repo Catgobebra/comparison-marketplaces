344bf89028b45c8969a43d993ba7074c149e96c0
Catgobebra <elderlyjmih@gmail.com>
2025-09-30 16:53:04 +0500

    feat: add check product id

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,3 +1,4 @@
+/* global chrome */
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
@@ -7,25 +8,83 @@ import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import Badge from '@mui/material/Badge';
 import InventoryIcon from '@mui/icons-material/Inventory';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+import { Link } from 'react-router-dom';
 
 function Main(){
 
     const [products,setProducts] = useState([])
     const [currentLink, setCurrentLink] = useState('');
 
-    const webLink = "https://learn.javascript.ru/regexp-introduction" 
-    const isOzonProduct = /https:\/\/www\.ozon\.ru\/product\/.+/gi.test(webLink);
+    const isOzonProduct = /https:\/\/www\.ozon\.ru\/product\/.+/i
+    const isCurrentSkuOzon = /\/product\/[^\/]+\-(\d{9,})(?:\/|\?|$)/i
+
+    const [currentUrl, setCurrentUrl] = useState('');
+
+    const [isProduct, setIsProduct] = useState(false);
+
+    const [open, setOpen] = React.useState(false);
+
+    const [snackbar, setSnackbar] = useState({
+        open: false,
+        severity: "success",
+        message: ""
+    });
+
+
+    const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+        return;
+        }
+        setSnackbar(prev => ({...prev, open: false}));
+    };
+
+
+    /* useEffect(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0]?.url) {
+            setCurrentUrl(tabs[0].url)
+            setIsProduct(isOzonProduct.test(tabs[0].url))
+        }
+        });
+    }, []); */
+
+    const checkProduct = (url) => {
+        return isOzonProduct.test(url) && isCurrentSkuOzon.test(url);
+    }
     
     const addProduct = () => {
-        setProducts([...products,currentLink])
-        setCurrentLink('')
+        const isValidProduct = checkProduct(currentLink) && !products.includes(currentLink.match(isCurrentSkuOzon)[1]);
+
+        if (isValidProduct) {
+            setProducts([...products, currentLink.match(isCurrentSkuOzon)[1]]);
+            setSnackbar({
+                open: true,
+                severity: "success",
+                message: "Товар успешно добавлен"
+            });
+        } else {
+            setSnackbar({
+                open: true,
+                severity: "error",
+                message: "Неверная или пустая ссылка"
+            });
+        }
+
+        setCurrentLink('');
+
+
     }
 
     const handleDelete = (productToDelete) => {
@@ -35,14 +94,14 @@ function Main(){
     const handleInputChange = (event) => {
         setCurrentLink(event.target.value); 
     };
-
+    
     return(
         <>
-        <Container maxWidth="lg" style={{display: 'flex', alignItems : 'center', justifyContent : 'center', flexDirection : 'column'}}>
-         <Box style={{padding : '100px'}}>
-            <Box style={{display: 'flex', gap: 20, position : 'relative'}}>
+        <Container maxWidth="lg" style={{ minWidth : '400px', display: 'flex', alignItems : 'center', justifyContent : 'center', flexDirection : 'column'}}>
+         <Box style={{padding : '10px'}}>
+            <Box style={{display: 'flex', gap: 20, position : 'relative', marginTop: '50px'}}>
             <Badge badgeContent={products.length} color="primary" style={{position : 'absolute', top : '-50px', right : '0'}}>
-            <InventoryIcon color="action" />
+            <ShoppingBasketIcon color="action" />
             </Badge>
             <TextField
             id="filled-search"
@@ -53,11 +112,21 @@ function Main(){
             onChange={handleInputChange}
             />
             <Button variant="contained" onClick={addProduct}>Добавить</Button>
-            {isOzonProduct &&
-            <Fab color="primary" aria-label="add">
-            <AddIcon />
-            </Fab>
+            {isProduct &&
+                (<Fab style={{minWidth: '56px'}} color="primary" aria-label="add">
+                <AddIcon />
+                </Fab>)
             }
+            <Snackbar open={snackbar.open} autoHideDuration={2000} onClose={handleClose}>
+            <Alert
+                onClose={handleClose}
+                severity={snackbar.severity}
+                variant="filled"
+                sx={{ width: '100%' }}
+            >
+                {snackbar.message}
+            </Alert>
+            </Snackbar>
             </Box>
             <Box>
                 <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
@@ -74,12 +143,15 @@ function Main(){
                     </IconButton>
                 }
                 >
+                <InventoryIcon color="action" />
                 <ListItemText primary={`${value}`} />
                 </ListItem>
                 ))}
                 </List>
             </Box>
-            <Button variant="contained">Перейти</Button>
+            <Button variant="contained">
+                Перейти
+            </Button>
          </Box>
         </Container>
         </>
@@ -87,3 +159,5 @@ function Main(){
 }
 
 export default Main
+
+

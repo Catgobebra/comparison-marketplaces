60c82989d3c86df03eb49937760461a19445eebf
данил <@DESKTOP-H5O83I6>
2025-12-18 15:16:59 +0500

    feat: add styles for widgets

---
+++ src/Components/Widgets/CategoriesList/styles.jsx
@@ -0,0 +1,13 @@
+export const listElement = {
+    fontFamily: "Nunito",
+    fontWeight: 400,
+    fontStyle: "Regular",
+    fontSize: "8px",
+    lineHeight: 1,
+    letterSpacing: "0%",
+    textAlign: "center",
+    verticalAlign: "middle",
+    wordWrap: "break-word",
+    overflowWrap: "break-word"
+
+}

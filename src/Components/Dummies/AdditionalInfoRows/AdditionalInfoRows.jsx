d594ba5b9f5d0186bde24f5bf3779d8b24dcf003
Catgobebra <elderlyjmih@gmail.com>
2026-01-10 15:30:49 +0500

    fix: hard to read hyperlinks to the product page on the comparison page with dark theme

--- src/Components/Dummies/AdditionalInfoRows/AdditionalInfoRows.jsx
+++ src/Components/Dummies/AdditionalInfoRows/AdditionalInfoRows.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { StyledTableRow, StyledTableCell } from "../../UI/StyledComponents/StyledComponents";
+import { useColorScheme } from "@mui/material/styles";
+import Link from '@mui/material/Link';
 
 export default function AdditionalInfoRows({ productsInfo,rankItems }) {
+  const { mode, setMode } = useColorScheme();
   return (
     <>
       <StyledTableRow>
@@ -44,14 +47,14 @@ export default function AdditionalInfoRows({ productsInfo,rankItems }) {
         {productsInfo.map((product, index) => (
           <StyledTableCell key={index} align="center">
             {product.productUrl ? (
-              <a
+              <Link
                 href={product.productUrl}
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{ fontSize: "0.8em" }}
               >
                 Перейти к товару
-              </a>
+              </Link>
             ) : (
               "—"
             )}
